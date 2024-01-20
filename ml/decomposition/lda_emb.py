bd0c849d6b045cf9369b1a7821e73dc26985e3ae
Minyus <Minyus@github.com>
2024-01-21 01:18:56 +0800

    Accept 2 numpy arrays to fit LDA

--- ml/decomposition/lda_emb.py
+++ ml/decomposition/lda_emb.py
@@ -1,14 +1,14 @@
 import numpy as np
+from numpy.testing import assert_array_almost_equal
 import polars as pl
-from polars.testing import assert_frame_equal
 from sklearn.decomposition import LatentDirichletAllocation
 
 
 class LDAEmb:
     def __init__(
         self,
-        col_1,
-        col_2,
+        col_1="col_1",
+        col_2="col_2",
         emb_col_format="{}_{}_e{:03d}",
         minimize_sort=False,
         **kwargs,
@@ -38,37 +38,38 @@ class LDAEmb:
         cooccurence_2darr = cooccurence_df.drop(col_1).to_numpy()
         return cooccurence_df, cooccurence_2darr
 
-    def fit(self, df):
+    def fit_transform(self, x, y):
+        self.fit(x, y)
+        return self.transform(x)
+
+    def fit(self, x, y):
+        assert x.shape == y.shape
+        df = pl.DataFrame(
+            {
+                self.col_1: x,
+                self.col_2: y,
+            }
+        )
         self.cooccurence_df, self.cooccurence_2darr = self.to_cooccurence_matrix(df)
-        self._lda.fit(self.cooccurence_2darr)
-
-    def transform(self, df):
-        cooccurence_df, cooccurence_2darr = self.to_cooccurence_matrix(df)
-        return self.transform_cooccurence(cooccurence_df, cooccurence_2darr)
-
-    def fit_transform(self, df):
-        self.fit(df)
-        return self.transform_cooccurence(self.cooccurence_df, self.cooccurence_2darr)
-
-    def transform_cooccurence(self, cooccurence_df, cooccurence_2darr):
-        col_1 = self.col_1
-        col_2 = self.col_2
-        emb_col_format = self.emb_col_format
-
-        emb_2darr = self._lda.transform(cooccurence_2darr)
-
+        self.emb_2darr = self._lda.fit_transform(self.cooccurence_2darr)
         emb_col_list = [
-            emb_col_format.format(col_1, col_2, i) for i in range(emb_2darr.shape[1])
+            self.emb_col_format.format(self.col_1, self.col_2, i)
+            for i in range(self.emb_2darr.shape[1])
         ]
-        emb_df = pl.DataFrame(emb_2darr, schema=emb_col_list)
-        emb_df = emb_df.with_columns([cooccurence_df[col_1]])
+        emb_df = pl.DataFrame(self.emb_2darr, schema=emb_col_list)
+        self.emb_df = emb_df.with_columns([self.cooccurence_df[self.col_1]])
+
+    def transform(self, x):
+
+        df = pl.DataFrame({self.col_1: x})
+
         out_df = df.join(
-            emb_df,
-            on=col_1,
+            self.emb_df,
+            on=self.col_1,
             how="left",
         )
-        out_df = out_df.drop([col_1, col_2])
-        return out_df
+        out_df = out_df.drop([self.col_1])
+        return out_df.to_numpy()
 
 
 if __name__ == "__main__":
@@ -77,34 +78,21 @@ if __name__ == "__main__":
     num_cats_1 = 7
     num_cats_2 = 5
     n_components = 3
-    col_1 = "col_1"
-    col_2 = "col_2"
 
     rng = np.random.RandomState(0)
-    df_dict = {
-        "index": np.arange(num_rows),
-        col_1: rng.randint(num_cats_1, size=num_rows),
-        col_2: rng.randint(num_cats_2, size=num_rows),
-    }
-
-    df = pl.DataFrame(df_dict)
+    x = rng.randint(num_cats_1, size=num_rows)
+    y = rng.randint(num_cats_2, size=num_rows)
 
     ldae = LDAEmb(
-        col_1=col_1,
-        col_2=col_2,
         n_components=n_components,
         learning_method="batch",
         random_state=rng,
     )
 
-    fit_transformed_df = ldae.fit_transform(df)
-    assert fit_transformed_df.width == df.width - 2 + n_components
-    assert len(fit_transformed_df) == len(df)
-    print(df)
-    print(fit_transformed_df)
+    fit_transformed_2darr = ldae.fit_transform(x, y)
+    assert fit_transformed_2darr.shape == (x.shape[0], n_components)
 
-    transformed_df = ldae.transform(df)
-    assert transformed_df.width == df.width - 2 + n_components
-    assert len(transformed_df) == len(df)
+    transformed_2darr = ldae.transform(x)
+    assert transformed_2darr.shape == (x.shape[0], n_components)
 
-    assert_frame_equal(transformed_df, fit_transformed_df)
+    assert_array_almost_equal(fit_transformed_2darr, transformed_2darr)
