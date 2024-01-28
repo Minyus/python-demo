68bc23a36db97203417c01760dfdcb0c48eabf5e
Minyus <Minyus@github.com>
2024-01-28 13:50:06 +0800

    Move if __name__

--- ml/decomposition/lda_emb.py
+++ ml/decomposition/lda_emb.py
@@ -200,85 +200,83 @@ class LDAEmbDf:
             return out_df
 
 
-def test_ldaemb():
-    """Test LDAEmb"""
-    from numpy.testing import assert_array_almost_equal
+if __name__ == "__main__":
 
-    num_rows = 100
-    num_cats_1 = 7
-    num_cats_2 = 5
-    n_components = 3
+    def test_ldaemb():
+        """Test LDAEmb"""
+        from numpy.testing import assert_array_almost_equal
 
-    rng = np.random.RandomState(0)
-    x = rng.randint(num_cats_1, size=num_rows)
-    y = rng.randint(num_cats_2, size=num_rows)
+        num_rows = 100
+        num_cats_1 = 7
+        num_cats_2 = 5
+        n_components = 3
 
-    ldae = LDAEmb(
-        n_components=n_components,
-        random_state=rng,
-    )
+        rng = np.random.RandomState(0)
+        x = rng.randint(num_cats_1, size=num_rows)
+        y = rng.randint(num_cats_2, size=num_rows)
 
-    fit_transformed_2darr = ldae.fit_transform(x, y)
-    assert fit_transformed_2darr.shape == (x.shape[0], n_components)
-
-    transformed_2darr = ldae.transform(x)
-    assert transformed_2darr.shape == (x.shape[0], n_components)
-
-    assert_array_almost_equal(fit_transformed_2darr, transformed_2darr)
-
-    assert_array_almost_equal(
-        ldae.transform(np.array([-1])), np.array([[np.NaN] * n_components])
-    )
-
-
-def test_ldaembdf():
-    """Test LDAEmbDf"""
-    from polars.testing import assert_frame_equal
-    from pandas.testing import assert_frame_equal as pd_assert_frame_equal
-
-    num_rows = 10
-    num_cats = 5
-    n_components = 3
-
-    rng = np.random.RandomState(0)
-
-    cat_cols = ["col_1", "col_2"]
-    df_dict = {
-        cat_col: rng.randint(num_cats, size=num_rows).astype(str)
-        for cat_col in cat_cols
-    }
-    df_dict["index"] = np.arange(num_rows)
-    df_dict["test_flag"] = df_dict["index"] < 3
-
-    for df in [pl.DataFrame(df_dict), pd.DataFrame(df_dict)]:
-        ldaed = LDAEmbDf(
-            cat_cols=None,
+        ldae = LDAEmb(
             n_components=n_components,
             random_state=rng,
         )
 
-        fit_transformed_df = ldaed.fit_transform(df)
-        # print(fit_transformed_df)
+        fit_transformed_2darr = ldae.fit_transform(x, y)
+        assert fit_transformed_2darr.shape == (x.shape[0], n_components)
 
-        transformed_df = ldaed.transform(df)
-        assert type(fit_transformed_df) == type(transformed_df), (
-            type(fit_transformed_df).__str__() + "\n!=\n" + type(transformed_df)
+        transformed_2darr = ldae.transform(x)
+        assert transformed_2darr.shape == (x.shape[0], n_components)
+
+        assert_array_almost_equal(fit_transformed_2darr, transformed_2darr)
+
+        assert_array_almost_equal(
+            ldae.transform(np.array([-1])), np.array([[np.NaN] * n_components])
         )
 
-        other_cols = [col for col in df.columns if col not in cat_cols]
-        if isinstance(df, pl.DataFrame):
-            assert_frame_equal(fit_transformed_df, transformed_df)
-            assert_frame_equal(
-                fit_transformed_df.select(other_cols), df.select(other_cols)
-            )
-        else:
-            pd_assert_frame_equal(fit_transformed_df, transformed_df)
-            pd_assert_frame_equal(
-                fit_transformed_df[other_cols], transformed_df[other_cols]
+    def test_ldaembdf():
+        """Test LDAEmbDf"""
+        from polars.testing import assert_frame_equal
+        from pandas.testing import assert_frame_equal as pd_assert_frame_equal
+
+        num_rows = 10
+        num_cats = 5
+        n_components = 3
+
+        rng = np.random.RandomState(0)
+
+        cat_cols = ["col_1", "col_2"]
+        df_dict = {
+            cat_col: rng.randint(num_cats, size=num_rows).astype(str)
+            for cat_col in cat_cols
+        }
+        df_dict["index"] = np.arange(num_rows)
+        df_dict["test_flag"] = df_dict["index"] < 3
+
+        for df in [pl.DataFrame(df_dict), pd.DataFrame(df_dict)]:
+            ldaed = LDAEmbDf(
+                cat_cols=None,
+                n_components=n_components,
+                random_state=rng,
             )
 
+            fit_transformed_df = ldaed.fit_transform(df)
+            # print(fit_transformed_df)
 
-if __name__ == "__main__":
+            transformed_df = ldaed.transform(df)
+            assert type(fit_transformed_df) == type(transformed_df), (
+                type(fit_transformed_df).__str__() + "\n!=\n" + type(transformed_df)
+            )
+
+            other_cols = [col for col in df.columns if col not in cat_cols]
+            if isinstance(df, pl.DataFrame):
+                assert_frame_equal(fit_transformed_df, transformed_df)
+                assert_frame_equal(
+                    fit_transformed_df.select(other_cols), df.select(other_cols)
+                )
+            else:
+                pd_assert_frame_equal(fit_transformed_df, transformed_df)
+                pd_assert_frame_equal(
+                    fit_transformed_df[other_cols], transformed_df[other_cols]
+                )
 
     test_ldaemb()
     test_ldaembdf()
